10ba62b0d9764d6d0db51a687bf735c66e4ccc0a
MOZGIII <mike-n@narod.ru>
2021-02-06 17:28:47 +0300

    Add usage example

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,22 @@
 //!
 //! Resolves the name via `getaddrinfo`, but more flexible than [`hyper`]
 //! standard resolver.
+//!
+//! ## Usage
+//!
+//!    ```
+//!    use hyper_system_resolver::{addr_info_hints, AddrInfoHints};
+//!
+//!    let addr_info_hints = AddrInfoHints {
+//!         address_family: addr_info_hints::AddressFamily::Inet6,
+//!    };
+//!    let system_resolve = hyper_system_resolver::system::System {
+//!        addr_info_hints: Some(addr_info_hints.into()),
+//!        service: None,
+//!    };
+//!    let http_connector = hyper::client::HttpConnector::new_with_resolver(system_resolve.resolver());
+//!    let client = hyper::client::Client::builder().build::<_, hyper::Body>(http_connector);
+//!    ```
 
 #![warn(missing_docs, clippy::all)]
 
