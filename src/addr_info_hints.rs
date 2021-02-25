1f4e307fa73b95dcf7732b34e4244a63efe77e6b
MOZGIII <mike-n@narod.ru>
2021-02-25 11:57:46 +0300

    Move the feature guard outside of the addr_info_hints mod

--- src/addr_info_hints.rs
+++ src/addr_info_hints.rs
@@ -3,8 +3,6 @@
 //! This is only required if you prefer a portable interface.
 //! You can use a raw [`dns_lookup::AddrInfoHints`] instead.
 
-#![cfg(feature = "addr-info-hints")]
-
 use dns_lookup::AddrFamily;
 
 #[cfg(unix)]
--- src/lib.rs
+++ src/lib.rs
@@ -29,6 +29,7 @@ extern crate tracing;
 #[macro_use]
 extern crate derive_builder;
 
+#[cfg(feature = "addr-info-hints")]
 pub mod addr_info_hints;
 pub mod background;
 pub mod system;
